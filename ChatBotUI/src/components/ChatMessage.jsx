b73797064863a5020f59766f2368a5b0827595a7
DaniMunteanu <dani.munteanu.dm@gmail.com>
2025-04-06 12:08:18 +0300

    Fixed bugs and styled chat form

--- ChatBotUI/src/components/ChatMessage.jsx
+++ ChatBotUI/src/components/ChatMessage.jsx
@@ -1,11 +1,12 @@
 import ChatbotIcon from "./ChatbotIcon"
+import ReactMarkdown from 'react-markdown'
 
 const ChatMessage = ({chat}) => {
   return (
     <div className={`message ${chat.role === "model" ? 'bot' : 'user'}-message`}>
         {chat.role === "model" && <ChatbotIcon />}
         <p className="message-text">
-        {chat.text} 
+        <ReactMarkdown>{chat.text}</ReactMarkdown> 
         </p>
     </div>
   )
